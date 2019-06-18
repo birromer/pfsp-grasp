4247e0e908de077a8fecd88c508b5fefaa160465
birromer <hummescmpa@gmail.com>
2019-06-18 13:06:54 -0300

    fixed makespan

--- heuristica.jl
+++ heuristica.jl
@@ -1,23 +1,22 @@
 
 function read_instances(filename::String)
-    sch = Array{UInt64,1}
+    sch = Array{Int64,1}
 
     stream = open(filename,"r")
 
     # splits first line by space and removes empy strings for better usage
     first_line = filter((x) -> x != "", split(readline(stream, keep=false), ' '))
-    number_machines::UInt64 = parse(UInt64,first_line[1])
-    number_jobs::UInt64 = parse(UInt64,first_line[2]) 
+    number_jobs::Int64     = parse(Int64,first_line[1])
+    number_machines::Int64 = parse(Int64,first_line[2]) 
 
-    t = zeros(UInt64, number_machines, number_jobs)
+    t   = zeros(Int64, number_jobs, number_machines)
+    sch = [x for x in 1:number_machines] # sets base schedule array with machine numbers in order
 
-    sch = [x for x in 1:number_machines]#1:number_machines  # sets base schedule array with machine numbers in order
-
-    for i in 1:number_machines
+    for i in 1:number_jobs
         machine_values = filter((x) -> x != "", split(readline(stream, keep=false), ' '))
-        for j = 2:2:length(machine_values) # iterates jumping the index items
+        for j = 2:2:length(machine_values) # iterates jumping the index values
             k = Int(j/2)
-            t[i,k] = parse(UInt64,machine_values[j])
+            t[i,k] = parse(Int64,machine_values[j])
         end
     end
 
@@ -26,20 +25,23 @@ end
 
 
 function makespan(sch, t) # computes the makespan of a certain solution
-    time_used::UInt64 = t[sch[1],1]
-    number_machines, number_jobs = size(t)
+    time_used::Int64 = t[sch[1],1]
+    number_jobs, number_machines = size(t)
 
-    t_line = zeros(UInt64, number_machines, number_jobs)
+    t = t' # armazenar resultados da transposta
+
+    t_line = zeros(Int64, number_machines, number_jobs)
     for i in 1:number_machines
         for j in 1:number_jobs
             t_line[i,j] = t[sch[i],j]
         end 
     end
 
-    finish_time = zeros(UInt64, number_machines, number_jobs)
+    finish_time = zeros(Int64, number_machines, number_jobs)
+    finish_time[1,1] = t_line[1,1]
 
-    for j in 1:number_jobs
-        finish_time[1,j] = t_line[1,j]
+    for j in 2:number_jobs
+        finish_time[1,j] = t_line[1,j] + finish_time[1,j-1]
     end
 
     for i in 2:number_machines
@@ -48,48 +50,55 @@ function makespan(sch, t) # computes the makespan of a certain solution
 
     for i in 2:number_machines
         for j in 2:number_jobs
-            top  = finish_time[i-1,j-1] + finish_time[i,j-1]
-            side = finish_time[i-1,j-1] + finish_time[i-1,j]
-            finish_time[i,j] = max(top,side)
+            top  = finish_time[i-1,j]
+            side = finish_time[i,j-1]
+            finish_time[i,j] = max(top,side) + t_line[i,j]
         end
     end
 
-    # for i = 1:number_machines
-    #     for j = 1:number_jobs
-    #         print(finish_time[i,j])
-    #         print(' ')
-    #     end
-    #     print('\n')
-    # end
+    for i = 1:number_machines
+        for j = 1:number_jobs
+            print(finish_time[i,j])
+            print(' ')
+        end
+        print('\n')
+    end
+
+
 
     return finish_time[number_machines,number_jobs]
 end
 
 
 
-# function construct(g::Function, alpha::Float32)
+function construct(g::Function, alpha::Float32)
 
-# end
+end
 
 
-# function GRASP(alpha::Float32, stop::UInt64)
-#     s_star::Float64 = 0
-#     s::Float64 = 0
-#     s_line::Float64 = 0
+function GRASP(alpha::Float32, stop::Int64)
+    s_star::Intt64 = 0
+    s::Int64 = 0
+    s_line::Int64 = 0
 
-#     for k in 1:stop
-#         s = construct(f, alpha)
-#         s_line = local_search(f,s)
-#         if f(s_line) <= f(s_star)
-#             s_star = s_line
-#         end
-#     end
-# end
+    for k in 1:stop
+        s = construct(makespan, alpha)
+        s_line = local_search(f,s)
+        if f(s_line) <= f(s_star)
+            s_star = s_line
+        end
+    end
+end
 
 function main()
     filename::String = ARGS[1]
+    
+    # timespans = {}
+
     sch, t = read_instances(filename)
     total_time = makespan(sch, t)
+
+
 
     println("total time = ", total_time)
 
