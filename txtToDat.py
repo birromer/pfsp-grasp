af5998871612e9cb66c26a43e46fe2bcf1e69b57
cixcore <cicinha.correa@gmail.com>
2019-06-19 00:51:16 -0300

    it is prettier now

--- txtToDat.py
+++ txtToDat.py
@@ -1,6 +1,6 @@
 import os
 
-filename = "VFR500"
+filename = "VFR10_15_1_Gap"
 file = open(filename + ".txt", "r")
 nums = file.read()
 i=0
@@ -14,7 +14,7 @@ nums_as_list = nums[j:i-1].split('  ')
 j = i+1
 
 f = open(filename + ".dat", "a")
-f.write("data;\n\n\n")
+f.write("data;\n\n")
 f.write("param num_jobs := " + nums_as_list[0]+ ";\n\n")
 f.write("param num_mach := " + nums_as_list[1] + ";\n\n")
 f.write("param T := \n")
@@ -30,14 +30,10 @@ for i in range(j, len(nums)):
         nums_as_list = [num for num in nums_as_list if num != '']
         nums_as_list_clean = [nums_as_list[i] for i in range(len(nums_as_list)) if i % 2 != 0]
         for k in range(1, m+1):
-            if l != n:
-                if k != m:
-                    last_line = str(l) + " " + str(k) + " " + nums_as_list_clean[k-1] + "\n"
+            if l == n and k == m:
+                last_line = str(l) + " " + str(k) + " " + nums_as_list_clean[k-1] + ";\n\n\n"
             else:
-                if k != m:
-                    last_line = str(l) + " " + str(k) + " " + nums_as_list_clean[k-1] + "\n"
-                else:
-                    last_line = str(l) + " " + str(k) + " " + nums_as_list_clean[k-1] + ";\n\n\n"
+                last_line = str(l) + " " + str(k) + " " + nums_as_list_clean[k-1] + "\n"
             f = open(filename + ".dat", "a")       
             f.write(last_line)
             f.close()
